241a4fe8dba40f210edac82b6567c2c6389f4996
Arun2812 <kumarrun7011@gmail.com>
2020-12-17 18:04:29 +0530

    double fab

---
+++ doublefabs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{   
+	float a,b,c;
+	a=2.5;
+	b=6.33;
+	c=9.4;
+	 
+	 printf("a=%0.3lf\n",floor(a));
+	 printf("b=%0.3lf\n",floor(b));
+	 printf("c=%0.3lf",floor(c));
+	 
+	
+	
+	
+}
